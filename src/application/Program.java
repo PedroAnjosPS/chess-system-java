d8cda1e44f597b2e82356d70ae40215ae05e155a
PedroAnjosPS <phpsanches@gmail.com>
2026-01-06 15:53:26 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		ChessMatch  chessMatch = new ChessMatch();
+		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
 			
